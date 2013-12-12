4c225589552dfa73711662b1b9e1316fbb923574
Arron Schaar <aschaar@gmail.com>
2013-12-12 12:07:42 -0800

    refactoring and added transitioning state

--- src/shiftbox.js
+++ src/shiftbox.js
@@ -1,12 +1,22 @@
 
 (function(){
 
+  function toggleOverlay(box, fn){
+    var section = xtag.query(box, 'section')[0];
+    if (section && box.tapclose){
+      section[fn](box.xtag.tapoverlay);
+    }
+  }
+
 	xtag.register('x-shiftbox', {
 		lifecycle: {
 			created: function(){
 				// flag to prevent opened and closed event from firing multiple times
 				// when transitionend is fired for each animated property
 				this.xtag.opened = this.hasAttribute('open');
+        this.xtag.tapoverlay = document.createElement('div');
+        this.xtag.tapoverlay.className = 'x-shiftbox-tapclose-overlay';
+
 			}
 		},
 		events:{
@@ -14,15 +24,17 @@
         var box = e.currentTarget;
         if (box.hasAttribute('open') && !box.xtag.opened){
           box.xtag.opened = true;
+          box.removeAttribute('transitioning');
           xtag.fireEvent(box, 'opened');
         }
         else if(!box.hasAttribute('open') && box.xtag.opened) {
           box.xtag.opened = false;
-          e.currentTarget.removeAttribute('data-opened');
+          box.removeAttribute('transitioning');
+          toggleOverlay(box, 'removeChild');
           xtag.fireEvent(box, 'closed');
         }
 			},
-      'tap:delegate(x-shiftbox[open] > section)': function(e){
+      'tap:delegate(x-shiftbox[open][tapclose] > section)': function(e){
         e.currentTarget.open = false;
       }
 		},
@@ -36,11 +48,17 @@
 			'open': {
 				attribute: { boolean: true },
         set: function(value){
-          if(value){
-            this.setAttribute('data-opened','');
+          if (value){
+            toggleOverlay(this, 'appendChild');
+          }
+          if (value != this.xtag.opened){
+            this.setAttribute('transitioning','');
           }
         }
-			}
+			},
+      'tapclose': {
+        attribute: { boolean: true }
+      }
 		},
 		methods: {
 			'toggle': function(){
